b9ead53408636747d236afd63da6bf713da34b5e
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-03-01 16:21:31 +0100

    add help

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ const handleMessage = Promise.coroutine(function* (message) {
   const origin = message.channel;
   let [[sender], [target]] = yield Promise.all([db.addUser(origin), db.getUser(recipient)]);
 
+  if (message.text === 'help') {
+    return rtm.sendMessage(`How to use me? Easy!
+If you just type a message, I will forward it to a random person, who's listening - this might even be you. If this happens, just try again. When they answer, you will see their answer as coming from a certain two-word name. In the future, when you want to message them, just make sure the first two words of your message are that two-word name, e.g. \`Totallyawesome Cuddlepanda Lorem Ipsum you know how this goes\`, and it will be delivered to them. If you skip the name, it will go to another randomly chosen person.
+
+That's it! There is no logging, and the database contains no data that can personally identify you without some seriously annoying research, which nobody is going to be doing, so feel free to be honest. You can see my brainz here, to make sure: https://github.com/benf86/friendBot
+`, origin);
+  }
+
   if (!target) {
     [target] = yield db.getListening();
     message.text = `    ${message.text}`;
   }
+
   /*console.log(
     `
 recipient: ${JSON.stringify(recipient)},
